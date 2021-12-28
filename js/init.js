4c99dcbcec3d7b2a9a6342bd6911628a67cf49d5
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2021-12-28 10:29:58 -0300

    Scroll centralizado ao iniciar a aplicação

--- js/init.js
+++ js/init.js
@@ -2,15 +2,20 @@ function init(){
     const URL = document.URL;
     const PARAMS = new URLSearchParams(URL.split('?')[1]);
 
-    const ITEMS = PARAMS.get('items') || 100;
+    const ITEMS = PARAMS.get('items') || 50;
+    const SIZE = PARAMS.get('size') || 30;
 
+    
     const HTML = document.querySelector('html');
 
     HTML.style.setProperty('--cells-columns-rows', ITEMS);
+    HTML.style.setProperty('--cell-size', SIZE + "px");
 
     construct(ITEMS);
     createCells(ITEMS);
 
     const CONTAINER = document.querySelector('.grid-container');
-    CONTAINER.scrollTo(CONTAINER.scrollWidth / 2 , CONTAINER.scrollHeight / 2);
+    
+    CONTAINER.scrollTop = (CONTAINER.scrollHeight - document.body.offsetHeight) / 2;
+    CONTAINER.scrollLeft = (CONTAINER.scrollWidth - document.body.offsetWidth) / 2;
 }

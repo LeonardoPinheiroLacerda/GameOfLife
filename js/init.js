6bdf4a06f5e32969bb6477a89dbf9cf611ef1e8a
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2021-12-28 00:24:46 -0300

    Melhor aproveitamento do espaço em tela

--- js/init.js
+++ js/init.js
@@ -2,15 +2,15 @@ function init(){
     const URL = document.URL;
     const PARAMS = new URLSearchParams(URL.split('?')[1]);
 
-    const ITEMS = PARAMS.get('items') || 40;
-
-    const SIZE = PARAMS.get('size') || 600;
+    const ITEMS = PARAMS.get('items') || 100;
 
     const HTML = document.querySelector('html');
 
     HTML.style.setProperty('--cells-columns-rows', ITEMS);
-    HTML.style.setProperty('--grid-size', SIZE + 'px');
 
     construct(ITEMS);
     createCells(ITEMS);
+
+    const CONTAINER = document.querySelector('.grid-container');
+    CONTAINER.scrollTo(CONTAINER.scrollWidth / 2 , CONTAINER.scrollHeight / 2);
 }
